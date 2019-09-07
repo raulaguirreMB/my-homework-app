52a52629f9d0522e05477edeaf0332d2b64f1336
Raul.Aguirre <raul.aguirre@mindbodyonline.com>
2019-09-07 16:12:09 -0700

    first push

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hello world</p>
+      </div>
+    );
+  }
+}
+
+export default App;
